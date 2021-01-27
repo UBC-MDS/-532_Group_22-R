0bb8a0b748a72ee69459500ef6999fbe81ef99f2
sbabicki <sbabicki@gmail.com>
2021-01-27 10:25:24 -0800

    Add source link

--- src/choropleth_leaflet.R
+++ src/choropleth_leaflet.R
@@ -1,6 +1,7 @@
 library(leaflet)
 library(tidyr)
 
+# RDS File Source: https://github.com/sbabicki/heatmapper/blob/master/geomap/data/CAN_1.rds
 provinces <- readRDS("data/processed/canadian_provinces.rds")
 leaflet(provinces) %>% 
   addTiles() %>% 
