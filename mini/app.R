c08b48ff42dfb0668e7c3d4f17c477c275271620
sbabicki <sbabicki@gmail.com>
2021-01-29 17:59:20 -0800

    Improve tab1 formatting

--- mini/app.R
+++ mini/app.R
@@ -44,16 +44,6 @@ DATA <- import_data()
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP, suppress_callback_exceptions=TRUE)
 
-app$callback(
-  output = output(id='cma_barplot', property='figure'),
-  params = list(input(id='metric_select', property='value')),
-  function(x){
-    ggplotly(
-      ggplot(gapminder, aes(x = year)) + geom_histogram(bins=10)
-    )
-  }
-)
-
 app$layout(
   htmlDiv(list(
     dccTabs(id='crime-dashboard-tabs', value='tab-1', 
@@ -76,30 +66,36 @@ app$callback(
         return(htmlDiv(generate_tab_1_layout())) 
     }
     else if (tab == 'tab-2') {
-      return(htmlDiv(generate_tab_1_layout()))
+      #return(htmlDiv(generate_tab_1_layout()))
     }
+  }
+)
+
+# Choropleth map, tab1
+app$callback(
+  output = output(id='choropleth', property='figure'),
+  params = list(input(id='metric_select', property='value')),
+  function(x){
+    renderLeaflet({leaflet() %>% addTiles()})
+    #ggplotly(
+    #  ggplot(gapminder, aes(x = year)) + geom_histogram(bins=10)
+    #)
   }
 )
 
 # CMA plot, tab1
 app$callback(
-  list(output('cma_barplot', 'children')),
-  list(input('metric_select', 'value'), 
-       input('violation_select', 'value')),
+  output('cma_barplot', 'children'),
+  list(
+    input('metric_select', 'value'), 
+    input('violation_select', 'value')
+  ),
   function(metric, violation) {
-    
-    #"""Create CMA barplot
-    #
-    #Returns
-    #-------
-    #html
-    #    altair plot in html format
-    #"""
-    
+
     df <- DATA %>%
-      filter(metric == !!sym(metric)) %>%
-      filter(`Violation Description` == !!sym(violation) %>%
-               filter(Geo_Level == "CMA"))
+      filter(Metric == !!sym(metric)) %>%
+      filter(Violation.Description == !!sym(violation)) %>%
+      filter(Geo_Level == "CMA")
     
     plot <- df %>%
       ggplot(aes(x = Value, y = Geography, tooltip = Value)) +
@@ -110,70 +106,24 @@ app$callback(
    ggplotly(plot, tooltip = 'Value')
 })
 
+get_dropdown_options <- function(col){
+  unique(col) %>% map(function(val) list(label = val, value = val))
+}
+
 app$callback(
-  output = output(id='choropleth', property='figure'),
-  params = list(input(id='metric_select', property='value')),
-  function(x){
-    metric <- "Rate per 100,000 population"
-    violation <- "Total, all violations" # or "Total, all violations [0]" 
-    year <- 2002
-    
-    provinces <- PROVINCES
-    
-    df <- DATA %>%
-      filter(Geo_Level == "PROVINCE") %>% 
-      filter(Metric == metric) %>%
-      filter(Violation.Description == violation) %>%
-      filter(Year == year)
-    
-    
-    province_data <- left_join(provinces, df, by=c("NAME"="Geography"))
-    
-    num_colours <- 13
-    bins <- round(seq(from=0, to=max(df$Value), length.out = num_colours), 0)
-    
-    # A few pallette options
-    #pallete <- "inferno"
-    pallete <- "RdYlBu"
-    #pallete <- topo.colors(num_colours)
-    #pallete <- colorRampPalette(c("#FF0000", "#000000", "#33FF00"))(num_colours)
-    
-    pal <- colorBin(pallete, domain = province_data$Value, bins = bins, reverse = TRUE)
-    
-    labels <- sprintf(
-      "<strong>%s</strong><br/>%g per 100k population",
-      province_data$NAME, province_data$Value
-    ) %>% lapply(htmltools::HTML)
-    
-    # Code based on tutorial: https://rstudio.github.io/leaflet/choropleths.html
-    #ggplotly() %>%
-    not_working <- province_data %>%
-      leaflet() %>% 
-      addTiles() %>% 
-      addPolygons(
-        fillColor = ~pal(Value),
-        weight = 1,
-        opacity = 1,
-        color = "black",
-        dashArray = "1",
-        fillOpacity = 0.7,
-        highlight = highlightOptions(
-          weight = 2,
-          color = "orange",
-          fillOpacity = 0.7,
-          bringToFront = TRUE),
-        label = labels,
-        labelOptions = labelOptions(
-          style = list("font-weight" = "normal", padding = "3px 8px"),
-          textsize = "15px",
-          direction = "auto")) %>%
-      addLegend(pal = pal, 
-                values = ~Value, 
-                opacity = 0.7, 
-                title = violation,
-                position = "bottomright")
-    ggplotly(ggplot(gapminder, aes(x=year))+geom_histogram(bins=10))
-    }
+  output('metric_select', 'options'),
+  list(input('crime-dashboard-tabs', 'value')),
+  function(...){
+    get_dropdown_options(DATA$Metric)
+  }
+)
+app$callback(
+  output('metric_select', 'value'),
+  list(input('crime-dashboard-tabs', 'value')),
+  function(...){
+    "Rate per 100,000 population"
+  }
 )
 
-app$run_server(debug=T, suppress_callback_exceptions = TRUE)
+
+app$run_server(debug=TRUE, suppress_callback_exceptions = TRUE)
--- src/tab1.R
+++ src/tab1.R
@@ -11,83 +11,63 @@ library(dashCoreComponents)
 library(dashBootstrapComponents)
 
 generate_tab_1_layout <- function(){
-#   """Generate tab 1 layout
-
-#   Returns
-#   -------
-#   dbc.Container
-#       Container with the html content of the page
-#   """    
-    #dropdown_height = 70    
+    #   """Generate tab 1 layout
+    
+    #   Returns
+    #   -------
+    #   dbc.Container
+    #       Container with the html content of the page
+    #   """    
+    dropdown_height <- 70    
+    
     dbcContainer(list(
         dbcRow(list(
             dbcCol(list(
                 dbcRow(list(
                     htmlDiv(list(
                         "Select Metric",
-                        dccDropdown(id="metric_select" #optionHeight=dropdown_height
-                                    )
-                    ) #style=list("width" = "100%"), #JOEL USES MAX-WIDTH
-                    )
-                    )),
-                    dbcRow(list(
-                        htmlDiv(
-                            list("Select Violation",
-                                        dccDropdown(
-                                            id="violation_select"
-                                            #optionHeight=dropdown_height,
-                                            
-                                        )
-                                 ) #style=list("width"= "100%"),
-                            )
-                        )
-                        ),
-                    dbcRow(list(
-                        htmlDiv(list(
-                                        "Select Violation Subcategory",
-                                        dccDropdown(id="subviolation_select",
-                                                    optionHeight=70)
-                                )
-                        ) #style=list("width" = "100%"),
-                        )
-                    )
-                ), 
-                #width="auto", 
-                style=list('padding-left'= '2%')
-                ), 
-                
-                # Column 2
-                dbcCol(list(
-                    dbcRow(list(
-                            htmlDiv("Violation Subcategory by Province"),
-                            htmlDiv(
-                                list(
-                                    dccGraph(id="choropleth")
-                                    #htmlImg( src="https://i.pinimg.com/originals/27/8e/ef/278eefb576915d43e85b7a467d8f709a.jpg",
-                                    #          width="100%",
-                                    )
-                                )
-                            )
-                        )
-                    ), 
-                    #width="auto", 
-                    style=list('padding-left' = '2%')
+                        dccDropdown(id = "metric_select", optionHeight=dropdown_height)
+                    ))
+                )),
+                dbcRow(list(
+                    htmlDiv(list(
+                        "Select Violation",
+                        dccDropdown(id = "violation_select", optionHeight = dropdown_height)
+                        ))
+                )),
+                dbcRow(list(
+                    htmlDiv(list(
+                        "Select Violation Subcategory",
+                        dccDropdown(id="subviolation_select", optionHeight = dropdown_height)
+                    ))
+                ))
+            ), 
+            #width="auto", 
+            style=list('padding-left'= '2%', "width" = "100%")
+            ), 
+            
+            # Column 2
+            dbcCol(list(
+                dbcRow(list(
+                    htmlDiv("Violation Subcategory by Province"),
+                    dccGraph(id="choropleth")
+                    ))
                 ),
-                
+                #width="auto", 
+                style=list('padding-left' = '2%')
+            ),
+            
             # Column 3
             dbcCol(list(
                 dbcRow(list(
                     htmlDiv("Violation Subcategory by CMA"),
                     dccGraph(id='cma_barplot', style = list('width'= '100%', 'height'= '600px'))
-                    )
-                    )
-                ),
-                #width="auto", 
-                style=list('padding-left' = '2%', 'padding-right' = '2%')
-                )
-            
+                ))
+            ),
+            #width="auto", 
+            style=list('padding-left' = '2%', 'padding-right' = '2%')
             )
-            )
+            ))
         ),
-        fluid = TRUE)
+    fluid = TRUE)
 }
