4184dddbce38e737365e73410d5589941ab98e46
Steffen Pentelow <steffen.pentelow@gmail.com>
2021-01-30 20:51:52 -0500

    Adjust column widths and column titles

--- src/tab1.R
+++ src/tab1.R
@@ -14,7 +14,7 @@ library(dashBootstrapComponents)
 #' @return dbcContainer Container with the html content of the page
 generate_tab_1_layout <- function(){
     
-    dropdown_height <- 70   
+    dropdown_height <- 60   
     start_year <- 1998
     end_year <- 2019
     year_range <- seq(start_year, end_year, 3)
@@ -56,35 +56,36 @@ generate_tab_1_layout <- function(){
                             value = 2019, 
                             dots = TRUE,
                             tooltip = list("placement" = "top"),
+                            included=FALSE,
                             marks = as.list(setNames(year_range, as.character(year_range)))
                             )
                     ), 
                     style = list("width" = "100%"))
                 ))
             ), 
-            width=3, 
+            width=2, 
             style=list('padding-left'= '2%', "width" = "100%")
             ), 
             
             # Column 2
             dbcCol(list(
                 dbcRow(list(
-                    htmlDiv("Violation Subcategory by Province"),
-                    dccGraph(id="choropleth", style = list('width'= '100%', 'height'= '600px'))
+                    htmlDiv("Violations by Province"),
+                    dccGraph(id="choropleth", style = list('width'= '100%', 'height'= '100%'))
                     ))
                 ),
-                width=4, 
+                width=5, 
                 style=list('padding-left' = '2%')
             ),
             
             # Column 3
             dbcCol(list(
                 dbcRow(list(
-                    htmlDiv("Violation Subcategory by CMA"),
+                    htmlDiv("Violations by CMA"),
                     dccGraph(id='cma_barplot', style = list('width'= '100%', 'height'= '600px'))
                 ))
             ),
-            width=5, 
+            width=4, 
             style=list('padding-left' = '2%', 'padding-right' = '2%')
             )
             ))
