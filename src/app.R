9269d0e8a62c09f4e3451a627970d1f2aa5b725f
Ify Anene <ifyanene@student.ubc.ca>
2021-01-30 22:32:00 -0500

    removed str replace function to test app

--- src/app.R
+++ src/app.R
@@ -24,7 +24,8 @@ import_data <- function() {
     drop_na(Value) %>%
     mutate(Geography = str_replace(Geography, " \\[[\\d|\\/]*\\]", "")) %>%
     mutate(Violation.Description = str_replace(Violation.Description, " \\[[\\d]*\\]", "")) %>%
-    mutate(Geo_Level = ifelse(Geography == "Prince Edward Island", str_replace(Geo_Level, "CMA", "PROVINCE"), Geo_Level)) 
+    mutate(Geo_Level = ifelse(Geography == "Prince Edward Island", 
+                              str_replace(Geo_Level, "CMA", "PROVINCE"), Geo_Level)) 
 }
 
 
